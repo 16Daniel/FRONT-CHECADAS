ba3f2f54e5f0a49f0a75c89131f7b1595911d406
Carlos R <carlos.r@operamx.com>
2026-05-18 11:16:23 -0600

    -

---
+++ src/environments/enviroments.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  planeacionApiConfig: {
+    url: "https://localhost:7171/api/",
+    apiKey: "QG15czNjcjN0QXAxazN5LXJ3LXAzZDFkMDUtMjAyNkA="
+  }
+};
